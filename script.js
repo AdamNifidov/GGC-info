601a46401e17f19e2ee7edcdffca5552959ac7d6
AdamNifidov <adyaaaa1001@gmail.com>
2026-03-09 12:15:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+﻿function setTheme(theme){
+document.body.className = theme
+localStorage.setItem("theme",theme)
+}
+
+function loadTheme(){
+let theme = localStorage.getItem("theme")
+if(theme){
+document.body.className = theme
+}
+}
+
+function setLang(lang){
+localStorage.setItem("lang",lang)
+location.reload()
+}
+
+function loadLang(){
+let lang = localStorage.getItem("lang") || "ru"
+
+document.querySelectorAll("[data-ru]").forEach(el=>{
+el.textContent = el.dataset[lang]
+})
+}
+
+window.onload = ()=>{
+loadTheme()
+loadLang()
+}
